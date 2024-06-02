3256acf6a06000e3f6b76cacf615fa03b25eba7b
connorharu <142368559+connorharu@users.noreply.github.com>
2024-06-02 14:14:50 -0300

    Update main.py
    adição da parte 2 do código e mudanças pequenas no cógido já anteriormente feito

--- specieslink/main.py
+++ specieslink/main.py
@@ -1,8 +1,8 @@
 import requests
 
 class species_link():
-    def __init__(self, api_key):
-        self.apikey = api_key # self é uma erferencia a propria instancia da classe;
+    def __init__(self, api_key): # __init__ é como um construtor
+        self.apikey = api_key # self é uma referencia a propria instancia da classe;
                               # api_key é o parametro que se espera receber ao instanciar a classe;
 
     # metadados básicos
@@ -31,8 +31,8 @@ class species_link():
 
     # dados de uma instituição por sigla ou identificador
     def get_institution_data(self, identifier, lang=None): # lang = none inicialmente indica que é opcional
-        url = f"https://specieslink.net/ws/1.0/ins/{identifier}/" 
-        params = {"apikey": self.apikey} # params recebe o parametro da URL da requisição HTTP GET
+        url = f"https://specieslink.net/ws/1.0/ins/{identifier}/" # f-string deixa adicionar expressões
+        params = {"apikey": self.apikey} # params é um dicionário; recebe o parametro da URL da requisição HTTP GET
                                          # onde a chave é apikey e o valor é self.apikey
         if lang:
             params["lang"] = lang # adicionando parametro lang ao dicionario params para filtragem
@@ -45,9 +45,9 @@ class species_link():
             return None
 
     # dados de uma coleção por sigla ou identificador
-    def get_collection_data(self, identifier, lang=None):
-        url = f"https://specieslink.net/ws/1.0/col/{identifier}/"
-        params = {"apikey": self.apikey} # params recebe o parametro da URL da requisição HTTP GET
+    def get_collection_data(self, identifier, lang=None): # lang = none inicialmente indica que é opcional
+        url = f"https://specieslink.net/ws/1.0/col/{identifier}/"  # f-string deixa adicionar expressões
+        params = {"apikey": self.apikey} # params é um dicionário; recebe o parametro da URL da requisição HTTP GET
                                          # onde a chave é apikey e o valor é self.apikey
         if lang:
             params["lang"] = lang # adicionando parametro lang ao dicionario params para filtragem 
@@ -62,13 +62,26 @@ class species_link():
     # dados de um conjunto de dados com um identificador especifico
     def get_dataset_info(self, id_dataset):
         url = f"https://specieslink.net/ws/1.0/dts/{id_dataset}/"
-        params = {"apikey": self.apikey} # params recebe o parametro da URL da requisição HTTP GET
-                                         # onde a chave é apikey e o valor é self.apikey
         # não tem parâmetro lang
-        response = requests.get(url, params=params) # HTTP GET para a URL requisitada passando os parametros
+        response = requests.get(url, params={"apikey": self.apikey}) # params recebe o parametro da URL da requisição HTTP GET
+                                                                     # onde a chave é apikey e o valor é self.apikey
         if response.status_code == 200: # solicitação feita com sucesso se ACK HTTP = 200
             data = response.json()
             return data
         else:
             print("erro ao obter informações do conjunto de dados")
+            return None
+    
+    # busca por registros de biodiversidade
+    def search_records(self, filters): # tem os filtros requisitados como parâmetro
+        url = "https://specieslink.net/ws/1.0/search"
+        params = {"apikey": self.apikey} # params recebe o parametro da URL da requisição HTTP GET
+                                         # onde a chave é apikey e o valor é self.apikey
+        params.update(filters)  # adiciona os filtros fornecidos ao dicionário de parâmetros
+        response = requests.get(url, params=params) # HTTP GET para a URL requisitada passando os parametros
+        if response.status_code == 200: # solicitação feita com sucesso se ACK HTTP = 200
+            data = response.json()
+            return data
+        else:
+            print("erro ao obter os registros de biodiversidade")
             return None
